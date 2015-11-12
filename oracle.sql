03187ea89703c3ff40a55662fbecb125e3740bf4
cro4t04n <l.villegas.allende@MW7XD9E8KUOS3O.dir.svc.accenture.com>
2015-11-12 13:05:56 -0300

    inicializando archivo

---
+++ oracle.sql
@@ -0,0 +1,18 @@
+SELECT * FROM SESSION_PRIVS; privilegios session
+select * from all_objects
+select * from all_source
+select * from user_tab_privs_recd where table_name = '...';
+
+
+SELECT *
+from all_tab_privs
+where table_name in
+  (select directory_name 
+   from dba_directories);
+   
+   
+   select 'Grant '||privilege||' on directory '||table_schema||'.'||table_name||' to '||grantee 
+from all_tab_privs 
+where table_name in (select directory_name from dba_directories);
+
+jdbc:oracle:thin:@bdb1:1554:fastb spring context
